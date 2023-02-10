7b3a7050c4a499ca13ece6865dec3c02b7017d00
david <davmwa90@gmail.com>
2023-02-10 16:47:36 +0300

    completed lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+#defining book class and using Macros 
+class Book 
+    attr_accessor :title, :author, :page_count, :genre 
+
+    def initialize(title)
+        @title=title  
+  
+    end 
+    def turn_page
+        p "Flipping the page...wow, you read fast!"
+    end 
+ 
+    end 
+
+ 
+    
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
-# Make your shoe class here!
+# Defining shoe class and utilizing Macros 
+class Shoe 
+    attr_accessor :brand, :color, :size, :material, :condition 
+    def initialize(brand)
+        @brand=brand 
+    end 
+    def cobble 
+        puts "Your shoe is as good as new!"
+        @condition="new" 
+    end 
+end 
